23fc0dee35c0681220a1667ef9ba837d1d81df82
Bundith <85783459+bundithdevER@users.noreply.github.com>
2022-04-04 09:44:30 +0700

    routing

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    const myhtml = `
-    <h1>Hello Bundith DEV</h1>
-    <h2>Hello</h2>
-    <h2>Hello2</h2>
-    <h2>Hello3</h2>
-    `
-    res.write(myhtml)
-    res.end()
+    const pathname = req.url
+    console.log(pathname);
+    if(pathname ==="/" || pathname ==="/home"){
+        const myhtml =`
+        <h1>Hello Homepage</h1>
+        <h1>Hello Bundith</h1>
+        `
+        res.end(myhtml)
+    }else if(pathname === "/product"){
+        res.end("<h1>Hello Product</h1>")
+    }else{
+        res.writeHead(404)
+        res.end("<h1>Not found</h1>")
+    }
 })
 server.listen(3000,'localhost',()=>{
     console.log("Start in port 3000");
